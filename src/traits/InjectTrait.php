cf879b4e7903d9acfa0d0f1b81140ce633dff92a
Linron <547042792@qq.com>
2025-12-09 10:52:55 +0800

    ..

--- src/traits/InjectTrait.php
+++ src/traits/InjectTrait.php
@@ -6,6 +6,9 @@ use linron\thinkdto\attributes\Inject;
 
 trait InjectTrait
 {
+    protected static array $resolvingObjects = [];
+    protected static array $resolvingClasses = [];
+
     /**
      * 解析 Inject 注解 依赖注入
      * @param $object
@@ -13,24 +16,53 @@ trait InjectTrait
      */
     public function parseInject($object): void
     {
-        $refObject = new \ReflectionObject($object);
-        foreach ($refObject->getProperties() as $refProperty) {
-            if ($refProperty->isDefault() && !$refProperty->isStatic()) {
-                $attrs = $refProperty->getAttributes(Inject::class);
-                if (!empty($attrs)) {
-                    if ($refProperty->getType() && !$refProperty->getType()->isBuiltin()) {
-                        $type = $refProperty->getType()->getName();
-                    }
+        $objectId = spl_object_hash($object);
+        $className = get_class($object);
 
-                    if (isset($type)) {
-                        $value = app()->make($type);
-                        if (!$refProperty->isPublic()) {
-                            $refProperty->setAccessible(true);
+        // 对象级别循环检测
+        if (isset(self::$resolvingObjects[$objectId])) {
+            return;
+        }
+
+        // 类级别循环检测
+        if (isset(self::$resolvingClasses[$className])) {
+            throw new \RuntimeException("Circular dependency detected for class: " . $className);
+        }
+
+        self::$resolvingObjects[$objectId] = true;
+        self::$resolvingClasses[$className] = true;
+
+        try {
+            $refObject = new \ReflectionObject($object);
+            foreach ($refObject->getProperties() as $refProperty) {
+                if ($refProperty->isDefault() && !$refProperty->isStatic()) {
+                    $attrs = $refProperty->getAttributes(Inject::class);
+                    if (!empty($attrs)) {
+                        if ($refProperty->getType() && !$refProperty->getType()->isBuiltin()) {
+                            $type = $refProperty->getType()->getName();
+
+                            // 检查要注入的类型是否正在解析中
+                            if (isset(self::$resolvingClasses[$type])) {
+                                // 延迟注入或跳过
+                                continue;
+                            }
+
+                            $value = app()->make($type);
+                            if (!$refProperty->isPublic()) {
+                                $refProperty->setAccessible(true);
+                            }
+                            $refProperty->setValue($object, $value);
+
+                            if (is_object($value)) {
+                                $this->parseInject($value);
+                            }
                         }
-                        $refProperty->setValue($object, $value);
                     }
                 }
             }
+        } finally {
+            unset(self::$resolvingObjects[$objectId]);
+            unset(self::$resolvingClasses[$className]);
         }
     }
 }
